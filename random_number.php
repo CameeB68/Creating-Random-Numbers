48dfb776e5462c6d8ab1661ee3a2185759e8006f
CameeB68 <camee.bunescu@cvusdk12.net>
2026-02-05 21:41:23 -0700

    Add files via upload

---
+++ random_number.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Random Number</title>
+</head>
+<body>
+
+<h1>Random Number Generator</h1>
+
+<?php
+// Generate a random number between 10 and 100
+$random = rand(10, 100);
+
+// Display the random number
+echo "<p>Your random number is: $random</p>";
+?>
+
+</body>
+</html>
